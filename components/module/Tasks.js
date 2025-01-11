9b22aba301e792c03644212fdfb61ae9c12110b1
ghmamadreza2 <ghmamadreza84@gmail.com>
2025-01-11 11:05:58 +0330

    i now handle update status task

--- components/module/Tasks.js
+++ components/module/Tasks.js
@@ -1,6 +1,18 @@
-import React from "react";
 import { RiMastodonLine } from "react-icons/ri";
-const Tasks = ({ data }) => {
+import { BiRightArrow, BiLeftArrow } from "react-icons/bi";
+const Tasks = ({ data, next, back, fetchTodos }) => {
+  const changeStatus = async ({ id, status }) => {
+    const res = await fetch("/api/todos", {
+      method: "PATCH",
+      body: JSON.stringify({ id, status }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+    if (data.status === "success") {
+      await fetchTodos();
+    }
+  };
+
   return (
     <div className="tasks">
       {data?.map((i) => (
@@ -8,6 +20,26 @@ const Tasks = ({ data }) => {
           <span className={i.status}></span>
           <RiMastodonLine />
           <h4>{i.title}</h4>
+          <div>
+            {back ? (
+              <button
+                className="button-back"
+                onClick={() => changeStatus({ id: i._id, status: back })}
+              >
+                <BiLeftArrow />
+                Back
+              </button>
+            ) : null}
+            {next ? (
+              <button
+                className="button-next"
+                onClick={() => changeStatus({ id: i._id, status: next })}
+              >
+                Next
+                <BiRightArrow />
+              </button>
+            ) : null}
+          </div>
         </div>
       ))}
     </div>
